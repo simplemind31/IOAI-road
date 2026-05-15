2cbcc5d98acda42578d79de858add5a0ea4712c9
simplemind31 <simplemind0331@gmail.com>
2026-05-15 09:56:54 -0400

    a

---
+++ nump.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = np.array([1, 2, 3, 4])
+b = np.array([1.0, 2.0, 3.0])
+c = np.zeros((2, 3))
+d = np.ones((3, 2))
+e = np.arange(0, 10, 2)       # [0, 2, 4, 6, 8]
+f = np.linspace(0, 1, 5) 
+print(f)
